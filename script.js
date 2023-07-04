1b1775f413b47871368e00374b8587e7728df8f4
zandenkoh <137964632+zandenkoh@users.noreply.github.com>
2023-07-04 12:58:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Check if the user is new
+if (!localStorage.getItem("visited")) {
+    showPopup();
+  }
+  
+  // Show the login popup
+  function showPopup() {
+    var popup = document.getElementById("loginPopup");
+    popup.style.display = "block";
+  }
+  
+  // Login function
+  function login() {
+    var name = document.getElementById("nameInput").value;
+    var password = document.getElementById("passwordInput").value;
+  
+    // Save name to local storage
+    localStorage.setItem("name", name);
+  
+    // Check password
+    if (password === "your_password_here") {
+      // Successful login, do something
+      alert("Login successful!");
+    } else {
+      // Incorrect password, display error message
+      alert("Incorrect password!");
+    }
+  
+    // Hide the popup
+    var popup = document.getElementById("loginPopup");
+    popup.style.display = "none";
+  
+    // Set the "visited" flag to prevent the popup from showing again
+    localStorage.setItem("visited", true);
+  }
+  
